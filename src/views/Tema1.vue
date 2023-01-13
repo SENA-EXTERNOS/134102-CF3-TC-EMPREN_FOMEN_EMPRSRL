300d843b045e31a9fc9a935ce7cd1ec37bc22906
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-01-13 17:03:42 -0500

    Update Tema1.vue

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -274,7 +274,7 @@
     .row.justify-content-center.align-items-center.mb-5
       .col-lg-8
         .titulo-sexto.color-acento-contenido(data-aos='fade-right')
-          p.mb-0 #[b Figura 1] #[i Fases del proceso administrativo] 
+          p.mb-0 #[b Figura 1] #[br] #[i Fases del proceso administrativo] 
       .col-lg-8.fnd-5.p-5
           figure
             img(src='@/assets/curso/temas/tema1/img-19.svg')  
