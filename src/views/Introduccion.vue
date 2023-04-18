0bbd02d35f668b0024bb0c545050cd342f09925b
Andrea Botello <andrea.botellor@misena.edu.co>
2023-04-18 16:00:13 -0500

    Videos y material

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-  .curso-main-container.introduccion
-    BannerInterno
-    .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+.curso-main-container.introduccion
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
     
         .titulo-principal.color-acento-contenido
           .titulo-principal__numero
@@ -21,8 +21,8 @@
     
         figure.mb-5
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-          figcaption Video: Leyenda del video
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/Aovcwkh084s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+
   
   </template>
 
